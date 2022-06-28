dd6e3daf46ec3bb7d589fdd85e2133d29e25d6b4
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2022-06-28 12:55:40 -0300

    Funcion Conversor decimal a binario

--- TpPromociónRPA/src/Conversor.java
+++ TpPromociónRPA/src/Conversor.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Conversor {
     public static void main(String[]args){
-        
+    
+        int numero;
+        String binario = "";
+        Scanner obj=new Scanner(System.in);
+        System.out.println("numero");
+        numero=obj.nextInt();
+
+        while (numero>0){
+
+            if(numero%2!=0){
+                binario =  '1'+binario ;
+                      }else{
+                if(numero%2==0){
+                    binario ='0'+ binario  ;
+                }
+            }
+            numero=numero/2;
+        }
+        System.out.println(binario);
+
     }
 }
+
